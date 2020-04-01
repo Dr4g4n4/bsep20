c2d6b287aa8ca7a9634c5296c601d38a172f8be7
Ivana <kovacevici404@gmail.com>
2020-04-01 04:42:30 +0200

    implementirano logovanje admina i dodata validacija polja na bekendu (nije testirano)

--- src/main/java/com/example/bsep/service/LoggingService.java
+++ src/main/java/com/example/bsep/service/LoggingService.java
@@ -1,5 +1,6 @@
 package com.example.bsep.service;
 
+import java.util.HashSet;
 import java.util.List;
 
 import com.example.bsep.model.Admin;
@@ -22,8 +23,13 @@ public class LoggingService implements UserDetailsService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public Admin loginUser(Admin u) {
-        Admin user = (Admin) loadUserByUsername(u.getUsername());
+    public Admin loginUser(String username, String password) {
+        if (!validateFields(username, password)) {
+            return null;
+        }
+
+        Admin user = (Admin) loadUserByUsername(username);
+
         if (user == null)
             return null;
         else if (user != null && user.isFirstLogin())  {
@@ -40,6 +46,57 @@ public class LoggingService implements UserDetailsService {
 
         return user;
 
+    }
+
+    private boolean validateFields(String username, String password ) {
+        return validateUsername(username) && validatePass(password);
+    }
+
+    private boolean validateUsername(String username) {
+        List<String> allUsernames = userRepo.findAllUsernames();
+        if (username.equals("")) {
+            return false;
+        }
+
+        for (String u : allUsernames) {
+            if (u.equals(username)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean validatePass(String pass) {
+        boolean ret = false;
+        boolean numCheck = false;
+        boolean upperCheck = false;
+        boolean lowerCheck = false;
+
+        if (pass.length() < 8) {
+           return ret;
+        }else {
+            for (Character c : pass.toCharArray()) {
+                if (Character.isDigit(c)) {
+                    numCheck = true;
+                    continue;
+                } else if (Character.isLowerCase(c)) {
+                    lowerCheck = true;
+                    continue;
+                } else if (Character.isUpperCase(c)) {
+                    upperCheck = true;
+                    continue;
+                }
+            }
+
+            if (!numCheck || !lowerCheck || !upperCheck) {
+                ret = false;
+            } else {
+                ret = true;
+            }
+        }
+
+        return ret;
     }
 
 }
